9cbf28093f82b4382f5a15b71316a5ac952d591b
roukaixin <a3427173515@163.com>
2025-02-09 12:00:37 +0800

    feat: 初始化项目

---
+++ src/main/java/com/roukaixin/cronvideos/CronVideosApplication.java
@@ -0,0 +1,13 @@
+package com.roukaixin.cronvideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronVideosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CronVideosApplication.class, args);
+    }
+
+}
